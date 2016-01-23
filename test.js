b5bc285485439a81ab912eae7f755f90f77bb3c0
Muntashir <kazimuntashir@gmail.com>
2016-01-23 18:55:15 -0500

    Put in missing require

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var sceney = require('./index').init(['Client Id','Client Secret']);
 
 sceney.rateImage(['https://upload.wikimedia.org/wikipedia/commons/b/be/Notre_Dame_dalla_Senna_crop.jpg','http://img01.deviantart.net/5f81/i/2008/312/7/8/paris____notre_dame_wp_by_superjuju29.jpg'], function(ratings) {
   
